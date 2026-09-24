9c1bc87e4673467ba507d0cafc7d67f4fb9e18ac
seeratulhaseenashaik <seeratulhaseenashaik@gmail.com>
2026-09-24 14:58:19 +0530

    adding print statements

--- digital_behaviour/pandas-intial.py
+++ digital_behaviour/pandas-intial.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import csv
 df=pd.read_csv('digital_behaviour.csv')
-df.head(5)
-df.tail(5)
-df.info()
-df.columns
-df['Instagram_Minutes']
-df[['Date','Instagram_Minutes']]
-df['Instagram_Minutes'].sum()
-df['Instagram_Minutes'].mean()
-df['Instagram_Minutes'].max()
+print(f'{df.head(5)}\n')
+print(f'{df.tail(5)}\n')
+print(f'{df.info()}\n')
+print(f'{df.columns}\n')
+print(f'{df['Instagram_Minutes']}\n')
+print(f'{df[['Date','Instagram_Minutes']]}\n')
+print(f'{df['Instagram_Minutes'].sum()}')
+print(f'{df['Instagram_Minutes'].mean()}')
+print(f'{df['Instagram_Minutes'].max()}')
 
